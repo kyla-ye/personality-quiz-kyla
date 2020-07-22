8dcd112e23d1c9f040b62dc097ff847dd0347dfc
Glitch (personality-quiz-starter-code) <none>
2020-07-22 15:06:24 +0000

    👜💳 Checkpoint ./script.js:19407098/153

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ var result4Score= 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
-
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
 
 
 
