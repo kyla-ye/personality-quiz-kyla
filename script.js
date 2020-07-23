317f178d7be6581a948d9eb4897c3a9dd76db19b
Glitch (personality-quiz-starter-code) <none>
2020-07-23 14:31:49 +0000

    🌘💪 Checkpoint ./script.js:19407098/761

--- script.js
+++ script.js
@@ -17,9 +17,20 @@ var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
 
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
 
-
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
 
 //#TODO: Add Event Listeners to your answer choice variables.
 q1a1.addEventListener("click", chill);
@@ -27,7 +38,10 @@ q1a2.addEventListener("click", straightforward);
 q1a3.addEventListener("click", fun);
 q1a4.addEventListener("click", kind);
 
-
+q2a1.addEventListener("click", straightforward);
+q2a2.addEventListener("click", kind);
+q2a3.addEventListener("click", fun);
+q2a4.addEventListener("click", chill);
 
 
 
